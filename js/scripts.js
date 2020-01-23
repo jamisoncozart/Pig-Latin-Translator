70b23067f2c201d1baabf4c6296ee80ed52b881f
Jamison Cozart and Kristina Hengster Tintor <kristina@>
2020-01-23 11:32:35 -0800

    Check for all consonants at beginning of word and move them to the end of the word

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,29 @@ $(document).ready(function() {
 
 function translate(sentence) {
   var vowels = ["a", "e", "i", "o", "u"];
-  for(var i = 0; i < vowels.length; i++) {
-    if(sentence[0] === vowels[i]) {
-      console.log("in if");
-      return sentence + "way";
-    } 
-  }; 
-  return "error";
+  if(sentence) {
+    for(var i = 0; i < vowels.length; i++) {
+      if(sentence[0] === vowels[i]) {
+        console.log("in if");
+        return sentence + "way";
+      }
+    };
+  
+    var firstVowelIndex = findFirstVowel(sentence, vowels);
+    var sentenceArr = sentence.split("");
+    var firstConsonants = sentenceArr.splice(0, firstVowelIndex);
+    var newSentence = sentence.slice(firstVowelIndex, sentence.length) + firstConsonants.join("") + "ay";
+    return newSentence;
+    
+  } else {
+    return "error";
+  }
+}
+
+function findFirstVowel(sentence, vowels) {
+  for(var i = 0; i < sentence.length; i++) {
+    if(vowels.includes(sentence[i])) {
+      return i;
+    }
+  };
 }
