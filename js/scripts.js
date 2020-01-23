0673735bd0969bf1ea9b4d69b3647c491a60518c
Jamison Cozart and Kristina Hengster Tintor <jamison@>
2020-01-23 13:41:52 -0800

    Translate entire sentences input by the user

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@ $(document).ready(function() {
   $("form#form").submit(function(event) {
     event.preventDefault();
     var sentence = $("#sentence").val();
-    $(".output").text(translate(sentence));
+    var wordArr = sentence.split(" ");
+    for(var i = 0; i < wordArr.length; i++) {
+      $(".output").append(" " + translate(wordArr[i]));
+    }
   });
 });
 
@@ -11,35 +14,40 @@ function translate(sentence) {
   if(sentence) {
     for(var i = 0; i < vowels.length; i++) {
       if(sentence[0] === vowels[i]) {
-        console.log("in if");
+        // console.log("in if");
         return sentence + "way";
       }
     };
-    var firstVowelIndex = findFirstVowel(sentence, vowels);
-    if((sentence[firstVowelIndex] === "u") && (sentence[firstVowelIndex - 1] === "q")) {
-      // firstConsonants += "qu";
-      var sentenceArr = sentence.split("");
-      var firstConsonants = sentenceArr.splice(0, firstVowelIndex + 1);
-      console.log(firstConsonants);
-      var newSentence = sentence.slice(firstVowelIndex + 1, sentence.length) + firstConsonants.join("") + "ay";
-      return newSentence;
+    // console.log(findFirstVowel(sentence, vowels));
+    if(findFirstVowel(sentence, vowels) === 0 || findFirstVowel(sentence, vowels)) {
+      var firstVowelIndex = findFirstVowel(sentence, vowels);
+      // console.log("in if");
+      if((sentence[firstVowelIndex] === "u") && (sentence[firstVowelIndex - 1] === "q")) {
+        var sentenceArr = sentence.split("");
+        var firstConsonants = sentenceArr.splice(0, firstVowelIndex + 1);
+        // console.log(firstConsonants);
+        var newSentence = sentence.slice(firstVowelIndex + 1, sentence.length) + firstConsonants.join("") + "ay";
+        return newSentence;
+      } else {
+        var sentenceArr = sentence.split("");
+        var firstConsonants = sentenceArr.splice(0, firstVowelIndex);
+        // console.log(firstConsonants);
+        var newSentence = sentence.slice(firstVowelIndex, sentence.length) + firstConsonants.join("") + "ay";
+        return newSentence;
+      } 
     } else {
-      var sentenceArr = sentence.split("");
-      var firstConsonants = sentenceArr.splice(0, firstVowelIndex);
-      
-      console.log(firstConsonants);
-      var newSentence = sentence.slice(firstVowelIndex, sentence.length) + firstConsonants.join("") + "ay";
-      return newSentence;
+      // console.log("in else");
+      return sentence;
     }
-  } else {
-    return "error";
   }
+  return "error";
 }
 
 function findFirstVowel(sentence, vowels) {
   for(var i = 0; i < sentence.length; i++) {
     if(vowels.includes(sentence[i])) {
       return i;
-    }
+    } 
   };
+  return false;
 }
